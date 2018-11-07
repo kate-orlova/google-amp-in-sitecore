b57a1c7a7c5a44b6a7dd4cc3bae0d8da250f79ad
Kate Orlova <kate.orlova@deleteagency.com>
2018-11-07 09:05:02 +0000

    Implemented ToGeneralUrl method

--- src/Foundation/MyFoundation/Extensions/StringExtensions.cs
+++ src/Foundation/MyFoundation/Extensions/StringExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace MyFoundation.Extensions
+﻿using System.Text.RegularExpressions;
+
+namespace MyFoundation.Extensions
 {
     public static class StringExtensions
     {
@@ -10,6 +12,15 @@
         public static bool NotEmpty(this string value)
         {
             return !string.IsNullOrEmpty(value) && !string.IsNullOrWhiteSpace(value);
+        }
+        public static string ToGeneralUrl(this string url)
+        {
+            if (url.IsNullOrEmpty())
+            {
+                return string.Empty;
+            }
+
+            return Regex.Replace(url, "^http(s)?:", "");
         }
     }
 }
