fdec7cbe1d7e22aad338b33a6bcf0dc26311c8e5
Kate Orlova <kate.orlova@deleteagency.com>
2018-11-11 13:19:29 +0000

    Implemented the Image class

--- src/Foundation/MyFoundation/Models/Image.cs
+++ src/Foundation/MyFoundation/Models/Image.cs
@@ -5,6 +5,26 @@ namespace MyFoundation.Models
     [Serializable]
     public class Image
     {
-  
+        public string Alt { get; set; }
+
+        public int Height { get; set; }
+
+        public int HSpace { get; set; }
+
+        public string Src { get; set; }
+
+        public int VSpace { get; set; }
+
+        public int Width { get; set; }
+
+        public Guid MediaId { get; set; }
+
+        public string Title { get; set; }
+
+        public virtual DateTime DateCreated { get; set; }
+
+        public virtual DateTime DateUpdated { get; set; }
+
+        public virtual string AdditionalParameters { get; set; }
     }
 }
