e90fc878e244110553e7e130d52e338dc480fffe
Kate Orlova <kate.orlova@deleteagency.com>
2018-11-11 13:25:03 +0000

    Implemented a HasExternalSource extension for the Image model

--- src/Foundation/MyFoundation/Extensions/ImageExtensions.cs
+++ src/Foundation/MyFoundation/Extensions/ImageExtensions.cs
@@ -1,6 +1,13 @@
-﻿namespace MyFoundation.Extensions
+﻿using System;
+using MyFoundation.Models;
+
+namespace MyFoundation.Extensions
 {
     public static class ImageExtensions
     {
+        public static bool HasExternalSource(this Image image)
+        {
+            return image.Src.NotEmpty() && image.MediaId == Guid.Empty;
+        }
     }
 }
