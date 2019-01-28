73289034dcd72cd4d236cb1ab5d8bca19a5d5acc
Kate Orlova <kate.orlova@deleteagency.com>
2019-01-28 20:40:45 +0000

    Implemented YouTubeVideo model

--- src/Feature/ContentComponents/Models/YouTubeVideo.cs
+++ src/Feature/ContentComponents/Models/YouTubeVideo.cs
@@ -1,6 +1,10 @@
-﻿namespace ContentComponents.Models
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+
+namespace ContentComponents.Models
 {
     public class YouTubeVideo
     {
+        [SitecoreField("YouTube Video Id")]
+        public virtual string VideoId { get; set; }
     }
 }
