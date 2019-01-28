a9925575d180c0dd17208032b01eab75353ab28b
Kate Orlova <kate.orlova@deleteagency.com>
2019-01-28 20:50:47 +0000

    Added an Autoplay property

--- src/Feature/ContentComponents/Models/YouTubeVideo.cs
+++ src/Feature/ContentComponents/Models/YouTubeVideo.cs
@@ -1,10 +1,13 @@
-﻿using Glass.Mapper.Sc.Configuration.Attributes;
+﻿using System;
+using Glass.Mapper.Sc.Configuration.Attributes;
 
 namespace ContentComponents.Models
 {
     public class YouTubeVideo
     {
-        [SitecoreField("YouTube Video Id")]
-        public virtual string VideoId { get; set; }
+        [SitecoreField("YouTube Video Id")] public virtual string VideoId { get; set; }
+
+        [SitecoreField("YouTube Video Autoplay")]
+        public virtual Boolean Autoplay { get; set; }
     }
 }
