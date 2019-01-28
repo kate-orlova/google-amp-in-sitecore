1790df582707d0f90aabe5bba06eacd0bb7b8baf
Kate Orlova <kate.orlova@deleteagency.com>
2019-01-28 20:28:32 +0000

    Implemented InlineImage model

--- src/Feature/ContentComponents/Models/InlineImage.cs
+++ src/Feature/ContentComponents/Models/InlineImage.cs
@@ -1,6 +1,10 @@
-﻿namespace ContentComponents.Models
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+
+namespace ContentComponents.Models
 {
     public class InlineImage
     {
+        [SitecoreField("Image")]
+        public virtual MyFoundation.Models.Image Image { get; set; }
     }
 }
