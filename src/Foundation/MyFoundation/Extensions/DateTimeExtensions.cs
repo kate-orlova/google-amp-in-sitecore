d34b34cb503ae0bb09e1ada7b1f955c36528ca30
Kate Orlova <kate.orlova@deleteagency.com>
2019-01-30 21:32:00 +0000

    Implemented a ToDateTimeOffset() method

--- src/Foundation/MyFoundation/Extensions/DateTimeExtensions.cs
+++ src/Foundation/MyFoundation/Extensions/DateTimeExtensions.cs
@@ -1,6 +1,14 @@
-﻿namespace MyFoundation.Extensions
+﻿using System;
+
+namespace MyFoundation.Extensions
 {
-    public class DateTimeExtensions
+    public static class DateTimeExtensions
     {
+        public static DateTimeOffset ToDateTimeOffset(this DateTime dateTime)
+        {
+            return dateTime.ToUniversalTime() <= DateTimeOffset.MinValue.UtcDateTime
+                ? DateTimeOffset.MinValue
+                : new DateTimeOffset(dateTime);
+        }
     }
 }
