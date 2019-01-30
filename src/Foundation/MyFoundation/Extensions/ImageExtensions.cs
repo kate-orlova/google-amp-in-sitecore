a4c0e0d26ff4a12ec8fabcadec297e25c151f842
Kate Orlova <kate.orlova@deleteagency.com>
2019-01-30 21:38:09 +0000

    Defined a GetMicrodata()

--- src/Foundation/MyFoundation/Extensions/ImageExtensions.cs
+++ src/Foundation/MyFoundation/Extensions/ImageExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using MyFoundation.Models;
+using Schema.NET;
 
 namespace MyFoundation.Extensions
 {
@@ -13,6 +14,11 @@ namespace MyFoundation.Extensions
             }
 
             return image.Src.NotEmpty() && image.MediaId == Guid.Empty;
+        }
+        public static ImageObject GetMicrodata(this Image image)
+        {
+            return new ImageObject();
+            
         }
     }
 }
