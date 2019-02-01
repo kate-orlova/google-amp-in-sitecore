099723e4819b3443a4ef21b4c1578ed60fcf4fa1
Kate Orlova <kate.orlova@deleteagency.com>
2019-02-01 19:06:40 +0000

    Inherited YouTubeVideo class from IMicrodata interface

--- src/Feature/ContentComponents/Models/YouTubeVideo.cs
+++ src/Feature/ContentComponents/Models/YouTubeVideo.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using Glass.Mapper.Sc.Configuration.Attributes;
+using MyFoundation.Interfaces;
+using Schema.NET;
 
 namespace ContentComponents.Models
 {
-    public class YouTubeVideo
+    public class YouTubeVideo : IMicrodata
     {
         [SitecoreField("YouTube Video Id")] public virtual string VideoId { get; set; }
 
         [SitecoreField("YouTube Video Autoplay")]
         public virtual Boolean Autoplay { get; set; }
+        public Thing GetMicrodata(IAuthorResolver authorResolver)
+        {
+            return new VideoObject();
+        }
     }
 }
