708b1974490de4a8231905f7d0a7d5fb171e4636
Kate Orlova <kate.orlova@deleteagency.com>
2019-02-02 15:55:31 +0000

    Added a splash image screen property

--- src/Foundation/MyFoundation/Models/YouTubeVideo.cs
+++ src/Foundation/MyFoundation/Models/YouTubeVideo.cs
@@ -1,16 +1,20 @@
-﻿using MyFoundation.Interfaces;
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using MyFoundation.Interfaces;
 using Schema.NET;
 using System;
-using Glass.Mapper.Sc.Configuration.Attributes;
 
 namespace MyFoundation.Models
 {
     public class YouTubeVideo : IMicrodata
     {
-        [SitecoreField("YouTube Video Id")] public virtual string VideoId { get; set; }
+        [SitecoreField("YouTube Video Id")]
+        public virtual string VideoId { get; set; }
 
         [SitecoreField("YouTube Video Autoplay")]
         public virtual Boolean Autoplay { get; set; }
+
+        [SitecoreField("Splash Image Screen")]
+        public virtual Image SplashImageScreen { get; set; }
 
         public Thing GetMicrodata(IAuthorResolver authorResolver)
         {
