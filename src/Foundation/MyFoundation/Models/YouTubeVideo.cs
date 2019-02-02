873ac7874f1e64b88234430ed192b5b47fa99942
Kate Orlova <kate.orlova@deleteagency.com>
2019-02-02 16:05:12 +0000

    Added author details to microdata

--- src/Foundation/MyFoundation/Models/YouTubeVideo.cs
+++ src/Foundation/MyFoundation/Models/YouTubeVideo.cs
@@ -24,7 +24,8 @@ namespace MyFoundation.Models
             {
                 Id = new Uri(@"https://www.youtube.com/watch?v=" + (VideoId ?? string.Empty), UriKind.RelativeOrAbsolute),
                 Thumbnail = splashImageScreen,
-                ThumbnailUrl = new Uri(SplashImageScreen?.Src ?? string.Empty, UriKind.RelativeOrAbsolute)           
+                ThumbnailUrl = new Uri(SplashImageScreen?.Src ?? string.Empty, UriKind.RelativeOrAbsolute),
+                Author = authorResolver?.GetAuthor() ?? new Organization()
             };
         }
     }
