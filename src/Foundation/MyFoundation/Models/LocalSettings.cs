181a5bc12ed354a4d70b9eaa2bb90a5281ad7fc4
Kate Orlova <kate.orlova@deleteagency.com>
2019-02-05 18:56:56 +0000

    Defined GetOrganizationMicrodata()

--- src/Foundation/MyFoundation/Models/LocalSettings.cs
+++ src/Foundation/MyFoundation/Models/LocalSettings.cs
@@ -1,6 +1,19 @@
-﻿namespace MyFoundation.Models
+﻿using Glass.Mapper.Sc.Configuration.Attributes;
+using Schema.NET;
+
+namespace MyFoundation.Models
 {
     public class LocalSettings
     {
+        [SitecoreField("Organisation Name")]
+        public virtual string OrganizationName { get; set; }
+
+        public Organization GetOrganizationMicrodata()
+        {
+            return new Organization
+            {
+                Name = OrganizationName
+            };
+        }
     }
 }
